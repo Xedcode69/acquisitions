aa166b630168b5639d5f30a2f612fb58ce9a618b
xed <xedcode69@gmail.com>
2025-12-25 21:29:55 +0530

    add logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import authRoute from './routes/auth.routes.js';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('combined', {stream: {write: (message) =>{
+  logger.info(message.trim());
+}} }));
+app.use(cookieParser());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
+  logger.info('Welcome to Acquisitions!');
   res.status(200).send('Welcome to Acquisions!');
 } );
 
--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,4 @@ import { config } from 'dotenv';
 config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
 
-export const {PORT, DB_URI} = process.env;
+export const {PORT, DB_URI, LOG_LEVEL} = process.env;
